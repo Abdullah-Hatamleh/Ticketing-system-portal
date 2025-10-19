0b8ca8146725396dbe0c70f8466be2b3780e8c1a
Abdullah Hatamleh <a.hatamleh@extensya.local>
2025-10-19 17:28:44 +0300

    before disaster

---
+++ src/components/ViewTicket.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+import { Modal } from "./Modal.vue";
+defineProps({
+    title: String,
+    priority: String,
+    description: String,
+    state: String,
+    categories: Array,
+    attachment: File
+})
+defineEmits({
+    
+})
+const isModalVisible = ref(false)
+</script>
+
+<template>
+    <Modal :show="isModalVisible" @close="$emit">
+        <template #title></template>
+        <template #default> test</template>
+    </Modal>
+</template>
